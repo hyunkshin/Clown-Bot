b4728467796695a021d732b500d32f5f199bd436
stephanieshin1 <49211123+stephanieshin1@users.noreply.github.com>
2019-09-11 15:21:49 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NjIwNDMxMTQ4NzQ0NTcyOTU1.XXXCvw.tFzz10lkAkAfQFwolc7d5g021Bg';
+
+const PREFIX = '!';
+
+var replies = [];
+var fs = require('fs');
+var replies = fs.readFileSync('messages.txt').toString().split("\n");
+
+//message.author
+
+bot.on('ready', () => {
+    console.log('This bot is online!');
+})
+
+bot.on('message', message=>{
+    msg = message.content.toLowerCase();
+
+    if (msg.includes("clown")) {
+
+        num = Math.floor(Math.random() * replies.length);
+        var text = replies[num];
+            
+        message.channel.send(text);
+    }
+
+})
+
+bot.login(token);
